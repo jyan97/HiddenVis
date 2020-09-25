cfea16280430a40bbe89d5a8c4eb43d78d23727c
jyan97 <ethanyan@163.com>
2020-09-25 11:27:27 -0500

    independence of SQL

--- flask_echarts/apps/__init__.py
+++ flask_echarts/apps/__init__.py
@@ -3,7 +3,7 @@
 from flask import Flask, render_template
 
 from apps.configs import config
-from apps.models import db
+# from apps.models import db
 
 # from apps.manager.density.views import density
 # from apps.manager.select_data.views import select_data
@@ -12,7 +12,7 @@ from apps.models import db
 def create_app():
     app = Flask(__name__)
     app.config.from_object(config)
-    with app.app_context():
-        db.init_app(app)
-        db.create_all()
+    # with app.app_context():
+    #     db.init_app(app)
+    #     db.create_all()
     return app
